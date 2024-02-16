f35b33312ffd1ea8d42c50d68db238d105a251fd
putn1k <skachok11@yandex.ru>
2024-02-16 17:18:32 +0300

    верстка проекта завершена

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -8,6 +8,19 @@ import {
 
 const initSliders = () => {
   initSlider( '.main-hero', sliderConfig.mainHero );
+  document.querySelectorAll( '.slider-gallery .slider' ).forEach( ( slider ) => {
+    const currentConfig = Object.assign( {
+      pagination: {
+        el: slider.querySelector( '.slider__pagination' ),
+        clickable: false,
+      },
+      navigation: {
+        prevEl: slider.querySelector( '[data-to-slide="prev"]' ),
+        nextEl: slider.querySelector( '[data-to-slide="next"]' ),
+      },
+    }, sliderConfig.gallery );
+    initSlider( slider, currentConfig );
+  } );
 };
 
 export {
